d12811e7d2fe3b4aa14ec9983faa94e86fbbfeed
lgrube <lgv9483@saintjoe.edu>
2015-04-01 17:02:22 -0500

    second part of javascript 101

---
+++ javascript2.js
@@ -0,0 +1,23 @@
+//luke Grube
+//4/1/15
+//write a funciton, is greater(str1, str2) that prints out the whichever of its parameter string lexicographically greater. show in action
+
+
+load("sortingCode.js");
+
+function isGreater(str1, str2) {
+	print(str1);
+	print(str2);
+	if(str1 > str2) {
+		print(str1 + " is greater than " + str2);
+	}
+	else if {
+		(str2 > str1) {
+			print(str2 + " is greater than " + str1);
+		}
+	}
+}
+
+
+var str1 = "greater";
+var str2 = "Lesser";
